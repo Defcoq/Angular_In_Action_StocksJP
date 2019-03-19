0cd5e409d4f4c4d9257fe0fc8a643555474db85a
Jean Pierre DeffoFotso <defcoq73@yahoo.fr>
2019-03-19 23:21:49 +0100

    first commit

---
+++ src/app/components/summary/summary.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'summary',
+  templateUrl: './summary.component.html',
+  styleUrls: ['./summary.component.css']
+})
+export class SummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  @Input() stock: any;
+
+  isNegative() {
+    if (!this.stock || this.stock.change >= 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+  isPositive() {
+    if (!this.stock || this.stock.change <= 0) {
+      return false;
+    }
+
+    return true;
+  }
+
+}
